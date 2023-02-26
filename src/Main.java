3d9d7c0979143b7d3a39255bdfc2a1058785b594
Isabelle <isagoubinskaya@gmail.com>
2023-02-26 19:20:54 +0100

    collections_map

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-import jdk.internal.jimage.BasicImageReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static <Mechanic> void main(String[] args) {
 
         Driver driver1 =new Driver("Sergei", true, 3) {
             @Override
@@ -54,5 +57,26 @@ public class Main {
             }
         };
         System.out.println("Driver " + driver3.getName() + " drives bus " + Bus.getModel() + " and will participate in the race.");
+        HashMap<Car, List<Mechanic>> mechanicsByCar;
+        mechanicsByCar = new HashMap<>();
+        Car car = new Car("Toyota", "Corolla", 2020);
+        List<Mechanic> mechanics = new ArrayList<>();
+        mechanicsByCar.put(car, mechanics);
+        Car carToFind = new Car("Toyota", "Corolla", 2020);
+        List<Mechanic> mechanicsForCar = mechanicsByCar.get(carToFind);
+
+        Object serviceStation = null;
+        HashMap<Object, Object> carMechanicsMap = null;
+        HashMap<Object, Object> carMechanics = null;
+        for (Map.Entry<Object, Object> entry : carMechanics.entrySet()) {
+            System.out.println("Car: " + car.getBrand() + " " + car.getModel());
+            System.out.println("Mechanics:");
+            for (Mechanic mechanic : mechanics) {
+                //System.out.println("- " + mechanic.getName() + " " + mechanic.getSurname());
+            }
+        }
+
+
+
     }
 }
